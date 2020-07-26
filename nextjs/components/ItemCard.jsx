2a5e2d27469545fe66bfb12504ab6261f490e3ce
bigdeal <bigdeal@smartstudy.co.kr>
2020-07-27 08:31:54 +0900

    init

---
+++ nextjs/components/ItemCard.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { observer, inject } from 'mobx-react';
+import { withTranslation } from "react-i18next";
+import { Carousel, Card, Tag } from 'antd';
+import { ShoppingCartOutlined, HeartOutlined } from '@ant-design/icons';
+
+const CardStyle = { width: 300, margin: '10px', display: 'inline-block', verticalAlign: 'top', textAlign: 'left', border: 'none', boxShadow: '0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)' };
+
+@inject('environment', 'auth')
+@observer
+class ItemCard extends React.Component {
+    render() {
+        const { environment, auth, i18n } = this.props;
+
+        return (
+            <Card
+                hoverable
+                style={CardStyle}
+                cover={
+                    <Carousel autoplay style={{
+                        textAlign: 'center',
+                        height: '300px',
+                        overflow: 'hidden',
+                    }}>
+                        <div>
+                            <img src="https://cdn.crewbi.com/images/goods_img/20190812/347455/347455_a_500.jpg" style={{ width: '100%', height: '300px', objectFit: 'cover' }} />
+                        </div>
+                        <div>
+                            <img src="https://usercontents-c.styleshare.io/images/20428940/700x432" style={{ width: '100%', height: '300px', objectFit: 'cover' }} />
+                        </div>
+                        <div>
+                            <img src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{ width: '100%', height: '300px', objectFit: 'cover' }} />
+                        </div>
+                    </Carousel>
+                }
+                actions={[
+                    <ShoppingCartOutlined key='addCart' />,
+                    <HeartOutlined key='like' />
+                ]}
+            >
+                <Tag color='magenta' style={{ position: 'absolute', top: '4px', left: '4px' }}>40% Sale</Tag>
+                <Tag style={{ position: 'absolute', top: '4px', right: '4px', margin: 0 }}>daesoo94</Tag>
+                <Card.Meta
+                    title="[another leeds] 쿨 썸머 후드 blouse"
+                    description={
+                        <div>
+                            <div style={{ textAlign: 'right' }}>
+                                <Tag style={{ textDecoration: 'line-through' }}>54,000 ₩</Tag>
+                                <Tag color='red' style={{ marginRight: 0 }}>37,000 ₩</Tag>
+                            </div>
+                            <div style={{ marginTop: '4px', fontSize: '0.8em' }}>
+                                완벽한 여리여리 핏의 꾸안꾸 느낌으로 편하고 캐주얼하게 입기 좋은 셔층 롱 원피스. 부드러운 폴리 면 원단을 사용해 시원하고 가볍게 입기 좋고 넉넉한 품과 롱 기장으로 체형 커버에도 딱이에요. 어나더리즈에서만 볼 수 있는 특별한 2가지 컬러로 보여드릴게요!
+                        </div>
+                        </div>
+                    }
+                />
+            </Card>
+        );
+    }
+}
+
+export default withTranslation('ItemCard')(ItemCard);
--- nextjs/pages/index.jsx
+++ nextjs/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Router from 'next/router';
 import { observer, inject } from 'mobx-react';
 import { withTranslation } from "react-i18next";
-import { Button, Select, Tag } from 'antd';
 import { initialize } from '../utils';
-import UserTest from '../components/UserTest';
+import ItemCard from '../components/ItemCard';
+import { Layout, Menu, Carousel } from 'antd';
+import { HomeOutlined, StarOutlined, TagsOutlined, AppstoreOutlined, ShoppingCartOutlined, HeartOutlined } from '@ant-design/icons';
+
+const HeaderMenuStyle = { height: '32px', width: '112px', textAlign: 'center' };
 
 @inject('environment', 'auth')
 @observer
@@ -13,59 +15,77 @@ class Home extends React.Component {
         super(props);
     }
 
-    login() {
-        Router.push('/connect/google');
-    }
-
-    logout() {
-        const { auth } = this.props;
-        auth.logout();
-    }
-
-    linkTo(url) {
-        Router.push(url);
-    }
-
-    changeLanguage(language) {
-        const { environment } = this.props;
-        environment.setLanguage(language);
-    }
-
     render() {
         const { environment, auth, i18n } = this.props;
 
         return (
-            <div>
-                <Button type='primary' onClick={() => { this.linkTo(`/new${environment.queryString}`) }}>
-                    New Page
-                    </Button>
-
-                {
-                    !auth.hasPermission &&
-                    <Button type='primary' onClick={this.login.bind(this)}>
-                        {i18n.t('login')}
-                    </Button>
-                }
-                {
-                    auth.hasPermission &&
-                    <Button type='danger' onClick={this.logout.bind(this)}>
-                        {i18n.t('logout')}
-                    </Button>
-                }
-
-                <Select value={environment.language} onChange={this.changeLanguage.bind(this)}>
-                    <Select.Option value="ko">Korean</Select.Option>
-                    <Select.Option value="en">English</Select.Option>
-                </Select>
-                {
-                    auth.hasPermission &&
-                    <>
-                        <Tag color='blue'>{auth.user.email}</Tag>
-                        <Tag color='magenta'>{auth.user.username}</Tag>
-                    </>
-                }
-                <UserTest test={'test'}></UserTest>
-            </div>
+            <Layout className="layout" style={{ maxWidth: '1280px', width: '100%', margin: 'auto' }}>
+                <div style={{ height: '240px' }}>
+                    <img src='https://cdn.searchenginejournal.com/wp-content/uploads/2018/04/e-commerce-store-1520x800.png' style={{ opacity: 0.2, width: '100%', height: '100%', objectFit: 'cover'}} />
+                </div>
+                <Layout.Header style={{ overflow: 'hidden', background: 'rgba(255,255,255,0.4)', padding: '0', height: '34px', lineHeight: '32px', boxShadow: '0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)'}}>
+                    <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']} style={{ height: '32px', backgroundColor: 'transparent' }}>
+                        <Menu.Item key="1" style={HeaderMenuStyle}>
+                            <HomeOutlined />{i18n.t('home')}
+                        </Menu.Item>
+                        <Menu.Item key="2" style={HeaderMenuStyle}>
+                            <StarOutlined />{i18n.t('new')}
+                        </Menu.Item>
+                        <Menu.Item key="3" style={HeaderMenuStyle}>
+                            <TagsOutlined />{i18n.t('sale')}
+                        </Menu.Item>
+                        <Menu.SubMenu key="category" icon={<AppstoreOutlined />} title={i18n.t('category')}>
+                            <Menu.Item key="4">Option 5</Menu.Item>
+                            <Menu.Item key="5">Option 6</Menu.Item>
+                            <Menu.Item key="7">Option 7</Menu.Item>
+                            <Menu.Item key="8">Option 8</Menu.Item>
+                        </Menu.SubMenu>
+                    </Menu>
+                </Layout.Header>
+                <Layout.Content>
+                    <div className="site-layout-content">
+                        <Carousel autoplay dotPosition='left' style={{
+                            textAlign: 'center',
+                            height: '500px',
+                            overflow: 'hidden',
+                            borderRadius: '20px',
+                            margin: '20px',
+                            boxShadow: '0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)'
+                        }}>
+                            <div>
+                                <img src='https://cdn.wallpapersafari.com/82/13/nlpBKP.jpg' style={{ width: '100%', height: '500px', objectFit: 'cover' }} />
+                            </div>
+                            <div>
+                                <img src='https://simracingsetup.com/wp-content/uploads/2020/02/F1-2020-All-Team-Desktop-Wallpaper.jpg' style={{ width: '100%', height: '500px', objectFit: 'cover' }} />
+                            </div>
+                            <div>
+                                <img src='https://wallpaperaccess.com/full/890113.png' style={{ width: '100%', height: '500px', objectFit: 'cover' }} />
+                            </div>
+                        </Carousel>
+                    </div>
+                    <div style={{ display: 'inline-block', textAlign: 'center' }}>
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                        <ItemCard />
+                    </div>
+                </Layout.Content>
+                <Layout.Footer style={{ textAlign: 'center', padding: '10 0px' }}>EveryWear ©2020 Created by SCH</Layout.Footer>
+            </Layout >
         );
     }
 }
